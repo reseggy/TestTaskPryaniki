7dd8335198fea6af5d9e96e975393fe073114b46
reseggy <reseggy@gmail.com>
2025-03-21 17:06:42 +0300

    Дополнения для публикации на gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { useSelector } from "react-redux";
 import type { RootState } from "./store/store";
 import Home from "./pages/Home";
